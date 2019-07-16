8a828829e38b63d1e3a070a7cc7e2e7316ebfebc
ChrisZhang95 <chris.zhang95@hotmail.com>
2019-07-16 15:14:09 -0400

    Searchbox and search functions are implemented

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
 import 'tachyons';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Hello greeting={'Hello React Nina'}/>, document.getElementById('root'));
+ReactDOM.render(
+  <App/>
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
